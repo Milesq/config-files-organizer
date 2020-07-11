e2c29d49a914c359064665152474f3cd31ce3d92
Milesq <wmilosz88@gmail.com>
2020-07-11 22:31:00 +0200

    Add help message

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,40 @@
 use configuer::Configuer;
 use serde::{Deserialize, Serialize};
+use std::env;
+
+mod messages;
+use messages::*;
 
 #[derive(Serialize, Deserialize, Clone, Default, Debug)]
 struct MyData {
-    user_name: String,
+    dirs: Vec<String>,
 }
 
 fn main() {
-    let mut config = Configuer::with_file("myIniFileName").on_create(|| {
-        println!("I see you open this app very first time, please pass your name: ...");
-
-        MyData {
-            user_name: "Default user name".into(),
-        }
+    let config = Configuer::with_file("myIniFileName").on_create(|| MyData {
+        dirs: vec!["Default user name".into()],
     });
 
-    println!("{:?}", config.data);
-    config.save();
+    let args: Vec<_> = env::args().collect();
+
+    let switch_is_set = |switches: &[&str]| {
+        args.iter()
+            .any(|el| switches.iter().any(|switch| switch == el))
+    };
+
+    if switch_is_set(&["-h", "--help"]) {
+        println!("{}", HELP_MSG);
+        return;
+    } else if switch_is_set(&["-m", "--manage-dirs"]) {
+        return;
+    }
+
+    let files = select_to_copy(config.data.dirs);
+    copy_files(files);
+}
+
+fn copy_files(_: Vec<String>) {}
+
+fn select_to_copy(_: Vec<String>) -> Vec<String> {
+    vec![]
 }
---
+++ src/messages.rs
@@ -0,0 +1,11 @@
+pub const HELP_MSG: &str = "Using:
+
+config-files
+
+Switches:
+
+-h,--help
+    Show help message
+
+-m,--manage-dirs
+    Shows CRUD view to manage dirs";
